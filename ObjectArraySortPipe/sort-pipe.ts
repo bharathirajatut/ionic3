42f9395bc204b7653b4543dd581330001c87314f
bharathirajatut <bharathirajatut@gmail.com>
2018-04-24 15:54:22 +0530

    object array sort ascending
    Angular 2 Object Array Sort Ascending Order

---
+++ ObjectArraySortPipe/sort-pipe.ts
@@ -0,0 +1,19 @@
+import { Pipe, PipeTransform } from "@angular/core";
+
+@Pipe({
+  name: "sort"
+})
+export class ArraySortPipe  implements PipeTransform {
+  transform(array: any[], field: string): any[] {
+    array.sort((a: any, b: any) => {
+      if (a[field] < b[field]) {
+        return -1;
+      } else if (a[field] > b[field]) {
+        return 1;
+      } else {
+        return 0;
+      }
+    });
+    return array;
+  }
+}
